3ccf2b98bee483f68013cb49df6e240ecb72668e
chenfeng <chenfeng@idengguo.com>
2020-05-25 11:24:57 +0800

    v1

--- routers/router.go
+++ routers/router.go
@@ -14,7 +14,7 @@ import (
 )
 
 func init() {
-	ns := beego.NewNamespace("/v3",
+	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/object",
 			beego.NSInclude(
 				&controllers.ObjectController{},
