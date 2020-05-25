b78ae99a4a622461b235d5cfdff8cb21d9b6eb6a
chenfeng <chenfeng@idengguo.com>
2020-05-25 11:24:23 +0800

    修改

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"ChasingLight/models"
 	_ "ChasingLight/routers"
-	"beego-api/util"
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	models.Init()
 	// 想模板中注册函数（首字母大写）
-	beego.AddFuncMap("ToUpper", util.FirstUpper)
 	beego.BConfig.WebConfig.Session.SessionOn = true
 }
 
